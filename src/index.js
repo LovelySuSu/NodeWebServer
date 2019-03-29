32f0298ce173fc537d22f4febf0f0a49aa0e2625
dingding <1060340716@qq.com>
2019-03-29 17:19:05 +0800

    抽取逻辑写为async await

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
 const http = require('http')
 const chalk = require('chalk')
 const path = require('path')
-const fs = require('fs')
+const route = require('./util/route')
 const config = require('./config/defaultConfig')
 
 const server = http.createServer((req,res) => {
 	const url = req.url
 	const filePath = path.join(config.root,url)
-	fs.stat(filePath,(error,stats) => {
-		if (error) {
-			res.statusCode = 404
-			res.setHeader('Content-Type','text/plain')
-			res.end(`${filePath} is not a dictionary or file`)
-			return
-		}
-		if(stats.isFile()) {
-			res.statusCode = 200
-			res.setHeader('Content-Type','text/plain')
-			fs.createReadStream(filePath).pipe(res)
-			return
-		} else if(stats.isDirectory()) {
-			fs.readdir(filePath,(error,files) => {
-				res.statusCode = 200
-				res.setHeader('Content-Type','text/plain')
-				res.end(files.join(','))
-			})
-		}
-	})
+	route(req,res,filePath)
 })
 
 server.listen(config.port,config.hostname,()=>{
