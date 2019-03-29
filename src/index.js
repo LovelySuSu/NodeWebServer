3e86e470d7d969dc661c087f5ead5bb88574c3b0
dingding <1060340716@qq.com>
2019-03-29 16:59:25 +0800

    读取文件夹和文件信息

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 const http = require('http')
 const chalk = require('chalk')
 const path = require('path')
+const fs = require('fs')
 const config = require('./config/defaultConfig')
 
 const server = http.createServer((req,res) => {
 	const url = req.url
 	const filePath = path.join(config.root,url)
-	res.statusCode = 200
-	res.setHeader('Content-Type','text/html')
-	res.end(filePath)
+	fs.stat(filePath,(error,stats) => {
+		if (error) {
+			res.statusCode = 404
+			res.setHeader('Content-Type','text/plain')
+			res.end(`${filePath} is not a dictionary or file`)
+			return
+		}
+		if(stats.isFile()) {
+			res.statusCode = 200
+			res.setHeader('Content-Type','text/plain')
+			fs.createWriteStream(filePath).pipe(res)
+			return
+		} else if(stats.isDirectory()) {
+			fs.readdir(filePath,(error,files) => {
+                res.statusCode = 200
+                res.setHeader('Content-Type','text/plain')
+                res.end(files.join(','))
+			})
+		}
+	})
 })
 
 server.listen(config.port,config.hostname,()=>{
